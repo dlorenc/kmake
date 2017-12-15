82d80ae920fd7971ca1b23de542c3430e20b7ab8
Matt Rickard <mrick@google.com>
2017-12-14 21:44:08 -0800

    Add naive file watcher

--- pkg/kmake/watch/watch.go
+++ pkg/kmake/watch/watch.go
@@ -1,8 +1,12 @@
 package watch
 
 import (
+	"path"
+	"path/filepath"
+
 	"github.com/pkg/errors"
 	"github.com/r2d4/kmake/pkg/kmake/docker"
+	"github.com/rjeczalik/notify"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,5 +19,25 @@ func Watch(imageName, dockerfile string) error {
 	deps := docker.GetDependencies(res)
 	logrus.Infof("Found dockerfile dependencies: %s", deps)
 
+	// TODO(@r2d4): make context configurable
+	dockerCtx := filepath.Dir(dockerfile)
+
+	c := make(chan notify.EventInfo, 1)
+	defer notify.Stop(c)
+	for _, dep := range deps {
+		dep := dep
+		go watchFile(path.Join(dockerCtx, dep), c)
+	}
+	ei := <-c
+	logrus.Infof("event: %s", ei)
+
+	return nil
+}
+
+func watchFile(path string, c chan notify.EventInfo) error {
+	logrus.Infof("Starting watch on %s", path)
+	if err := notify.Watch(path, c, notify.All); err != nil {
+		return errors.Wrap(err, "notify")
+	}
 	return nil
 }
